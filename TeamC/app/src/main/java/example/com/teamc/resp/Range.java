6427905730231474cc4c34c5080131be9c5c259e
ayijk <contact@ayijk.com>
2017-02-14 22:22:25 +0900

    add the response class for Range

--- TeamC/app/src/main/java/example/com/teamc/resp/Range.java
+++ TeamC/app/src/main/java/example/com/teamc/resp/Range.java
@@ -5,4 +5,29 @@ package example.com.teamc.resp;
  */
 
 public class Range {
+    ResultSet ResultSet;
+    Point[] Point;
+
+    class ResultSet {
+    }
+
+    class Point {
+        int Minute;
+        Prefecture Prefecture;
+
+        class Prefecture {
+            String Name;
+            String code;
+        }
+
+        Station Station;
+
+        class Station {
+            String Name;
+            String Type;
+            String code;
+        }
+
+    }
+
 }
